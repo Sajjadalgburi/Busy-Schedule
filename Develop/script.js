c1dde8e51b581b25246cafd999a85e147fa10955
Sajjadalgburi <dfgsajjad@gmail.com>
2023-12-10 15:26:24 -0700

    Aligned the header in the middle and added the time of the day

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,8 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  var TimeDisplayEl = $("#currentDay");
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +22,10 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  function displayTime() {
+    var rightNow = dayjs().format("MMM DD, YYYY [at] hh:mm:ss a");
+    TimeDisplayEl.text(rightNow);
+  }
+
+  setInterval(displayTime, 1000);
 });
