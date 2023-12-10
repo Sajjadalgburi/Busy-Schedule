dd425656319d54150d67b5b56beae82ae1a52a5c
Sajjadalgburi <dfgsajjad@gmail.com>
2023-12-10 15:34:45 -0700

    Put document ready function so the web can load up before code executes

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,8 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+$(document).ready(function () {
   var TimeDisplayEl = $("#currentDay");
 
   // TODO: Add a listener for click events on the save button. This code should
