648dfdd61f818ceab13d630865c4cc17cf9bed41
Sajjadalgburi <dfgsajjad@gmail.com>
2023-12-12 12:21:14 -0700

    Removed the exessive if statments and implemented color change for schedule

--- Develop/script.js
+++ Develop/script.js
@@ -4,27 +4,9 @@
 
 $(document).ready(function () {
   var TimeDisplayEl = $("#currentDay");
-  var hourTextEl = $(".hour");
-  var hour9 = $("#hour-9");
-  var hour10 = $("#hour-10");
-  var hour11 = $("#hour-11");
-  var hour12 = $("#hour-12");
-  var hour1 = $("#hour-1");
-  var hour2 = $("#hour-2");
-  var hour3 = $("#hour-3");
-  var hour4 = $("#hour-4");
-  var hour5 = $("#hour-5");
-  // var array = [
-  //   hour9,
-  //   hour10,
-  //   hour11,
-  //   hour12,
-  //   hour1,
-  //   hour2,
-  //   hour3,
-  //   hour4,
-  //   hour5,
-  // ];
+  var descriptionEl = $(".description");
+  var saveBtnEl = $(".saveBtn");
+  var timeBlockEl = $(".time-block");
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -33,120 +15,55 @@ $(document).ready(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+  // Add click event handler to all elements with class 'saveBtn'
+
+  $(".saveBtn").on("click", function () {
+    // Traverse up the DOM to find the closest ancestor with class 'time-block'
+    var timeBlock = $(this).closest(".time-block");
+    var description = $(".description").val();
+    console.log(description);
+
+    if (timeBlock.length) {
+      // Get the id attribute of the time block
+      var timeBlockId = timeBlock.attr("id");
+      console.log(timeBlockId);
+    }
+  });
+
+  function saveThis() {
+    var changeColorOfBtn = $(this).css("background", "orange");
+    localStorage.setItem("color", JSON.stringify(changeColorOfBtn));
+  }
+
+  saveBtnEl.click(saveThis);
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+  function checkHour() {
+    // Iterate over all div blocks with class 'time-block'
+    $(".time-block").each(function () {
+      const currentHour = dayjs().hour(); // Use the fixed time
+      // const currentHour = dayjs().hour(); // 24-hour format
 
-  function ChangeBlockColor() {
-    var currentDay = dayjs().hour();
+      // Get the hour from the id attribute of the current time block
+      var timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
 
-    // var currentDay = dayjs().set("hour", 15).set("minute", 0).set("second", 0);
-    // console.log(currentDay);
-
-    var elements = $(".time-block");
-
-    for (var i = 0; i < elements.length; i++) {
-      var element = elements[i];
-
-      var id = element.id;
-      var numericPart = parseInt(id.split("-")[1]);
-
-      if (numericPart === currentDay) {
-        hour9.addClass("present");
-      } else if (numericPart < currentDay) {
-        hour9.removeClass("present");
-        hour9.addClass("past");
+      if (timeBlockHour < currentHour) {
+        $(this).removeClass("present future");
+        $(this).addClass("past");
+      } else if (timeBlockHour === currentHour) {
+        $(this).removeClass("past future");
+        $(this).addClass("present");
       } else {
-        hour9.removeClass("present");
-        hour9.addClass("future");
+        $(this).removeClass("past present");
+        $(this).addClass("future");
       }
-
-      if (numericPart === currentDay) {
-        hour10.addClass("present");
-      } else if (numericPart < currentDay) {
-        hour10.removeClass("present");
-        hour10.addClass("past");
-      } else {
-        hour10.removeClass("present");
-        hour10.addClass("future");
-      }
-
-      if (numericPart === currentDay) {
-        hour12.addClass("present");
-      } else if (numericPart < currentDay) {
-        hour12.removeClass("present");
-        hour12.addClass("past");
-      } else {
-        hour12.removeClass("present");
-        hour12.addClass("future");
-      }
-
-      if (numericPart === currentDay) {
-        hour1.addClass("present");
-      } else if (numericPart < currentDay) {
-        hour1.removeClass("present");
-        hour1.addClass("past");
-      } else {
-        hour1.removeClass("present");
-        hour1.addClass("future");
-      }
-
-      if (numericPart === currentDay) {
-        hour2.addClass("present");
-      } else if (numericPart < currentDay) {
-        hour2.removeClass("present");
-        hour2.addClass("past");
-      } else {
-        hour2.removeClass("present");
-        hour2.addClass("future");
-      }
-
-      if (numericPart === currentDay) {
-        hour3.addClass("present");
-      } else if (numericPart < currentDay) {
-        hour3.removeClass("present");
-        hour3.addClass("past");
-      } else {
-        hour3.removeClass("present");
-        hour3.addClass("future");
-      }
-
-      if (numericPart === currentDay) {
-        hour4.addClass("present");
-      } else if (numericPart < currentDay) {
-        hour4.removeClass("present");
-        hour4.addClass("past");
-      } else {
-        hour4.removeClass("present");
-        hour4.addClass("future");
-      }
-
-      if (numericPart === currentDay) {
-        hour5.addClass("present");
-      } else if (numericPart < currentDay) {
-        hour5.removeClass("present");
-        hour5.addClass("past");
-      } else {
-        hour5.removeClass("present");
-        hour5.addClass("future");
-      }
-
-      if (numericPart === currentDay) {
-        hour11.addClass("present");
-      } else if (numericPart < currentDay) {
-        hour11.removeClass("present");
-        hour11.addClass("past");
-      } else {
-        hour11.removeClass("present");
-        hour11.addClass("future");
-      }
-    }
+    });
   }
-
-  ChangeBlockColor();
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
@@ -158,4 +75,5 @@ $(document).ready(function () {
   }
 
   setInterval(displayTime, 1000);
+  checkHour();
 });
