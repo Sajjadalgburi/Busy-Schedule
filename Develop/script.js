951451b1e3524d6ba9b8538d9facf406487969a2
Sajjadalgburi <dfgsajjad@gmail.com>
2023-12-14 16:18:30 -0700

    Displayed the items saved from local storage

--- Develop/script.js
+++ Develop/script.js
@@ -6,13 +6,6 @@ $(document).ready(function () {
 
   var items = [];
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-
   // Attach a click event handler to elements with the class "saveBtn"
   $(".saveBtn").on("click", function (event) {
     // Prevent the default behavior of the button (e.g., form submission)
@@ -70,19 +63,20 @@ $(document).ready(function () {
     });
   }
 
-  //
-  //
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
   function getItems() {
     return localStorage.getItem("hourly-calander");
   }
-
   function displaySavedItems() {
-    var items = getItems();
+    var storedItems = getItems();
+    var items = JSON.parse(storedItems) || [];
+
+    $.each(items, function (index, timeObject) {
+      var timeBlockId = timeObject.timeBlockId;
+      var descriptionVal = timeObject.descriptionVal;
+
+      // Update the text area with the descriptionVal
+      $("#" + timeBlockId + " textarea").val(descriptionVal);
+    });
   }
 
   //
